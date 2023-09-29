79fb84febff13e43f3dba35ca63c0a00489d4a62
AbatMac <hasan.oruc@ankabeta.com.tr>
2023-09-29 04:08:54 +0300

    meet date picker added

--- src/app/pages/meet-module/meetings/meeting.service.ts
+++ src/app/pages/meet-module/meetings/meeting.service.ts
@@ -9,11 +9,12 @@ import { Meeting,Note } from './meeting.model';
 
 
 export class MeetingService {
+
   private meetings: Meeting[] = [
-    new Meeting(1, 'Toplantı 1', new Date('2023-10-15'), 'Ofis A', ['Katılımcı 1', 'Katılımcı 2'],'www.toplanti1.com'),
-    new Meeting(1, 'Toplantı 2', new Date('2023-10-15'), 'Ofis B', ['Katılımcı 4', 'Katılımcı 5'],'www.toplanti2.com'),
-    new Meeting(1, 'Toplantı 3', new Date('2023-10-15'), 'Ofis C', ['Katılımcı 23', 'Katılımcı 7'],'www.toplanti3.com'),
-    new Meeting(1, 'Toplantı 4', new Date('2023-10-15'), 'Ofis D', ['Katılımcı 1', 'Katılımcı 2'],'www.toplanti4.com'),
+    new Meeting(1, 'Toplantı 1', new Date('2023-10-12'), 'Ofis A', ['Katılımcı 1', 'Katılımcı 2'],'www.toplanti1.com'),
+    new Meeting(1, 'Toplantı 2', new Date('2023-10-11'), 'Ofis B', ['Katılımcı 4', 'Katılımcı 5'],'www.toplanti2.com'),
+    new Meeting(1, 'Toplantı 3', new Date('2023-10-17'), 'Ofis C', ['Katılımcı 23', 'Katılımcı 7'],'www.toplanti3.com'),
+    new Meeting(1, 'Toplantı 4', new Date('2023-10-20'), 'Ofis D', ['Katılımcı 1', 'Katılımcı 2'],'www.toplanti4.com'),
     // Daha fazla toplantı ekleyebilirsiniz.
   ];
 
@@ -28,6 +29,10 @@ export class MeetingService {
       { content: 'Yapılabilecekler 1', category: 'Yapılabilecekler' },
       { content: 'Bilgi 1', category: 'Bilgi' },
     ]);
+  }
+
+  getMeetingsByDateRange(startDate: Date, endDate: Date): Meeting[] {
+    return this.meetings.filter(meeting => meeting.date >= startDate && meeting.date <= endDate);
   }
 
   getMeetingNotes(meeting: Meeting): Note[] {
