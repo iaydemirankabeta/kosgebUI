cc886a9ea3d1aa2e918302d17f7a54af31fec74d
AbatMac <hasan.oruc@ankabeta.com.tr>
2023-10-16 17:10:24 +0300

    edited frontend

--- src/app/pages/catalog/catalog.module.ts
+++ src/app/pages/catalog/catalog.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CKEditorModule } from 'ckeditor4-angular';
 import { FormsModule } from '@angular/forms';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
 import { ExtrasModule } from '../../_metronic/partials';
 import { PaginationService } from '../service/pagination.service';
 import { PaginationComponent } from 'src/app/modules/pagination/pagination.component';
@@ -15,14 +16,17 @@ import { ProductAddComponent } from './products/product-add/product-add.componen
 import { ShowcaseComponent } from './showcase/showcase.component';
 import { SearchComponent } from '../../pages/components/search/search.component';
 import { CurrencyComponent } from './currency/currency.component';
+import { MarketplacesComponent } from './marketplaces/marketplaces.component';
 
 
 @NgModule({
-  declarations: [CatalogComponent,ProductsComponent,CategoriesComponent,PaginationComponent, ProductAddComponent, ShowcaseComponent,SearchComponent, CurrencyComponent],
+  declarations: [CatalogComponent,ProductsComponent,CategoriesComponent,PaginationComponent, ProductAddComponent, ShowcaseComponent,SearchComponent, CurrencyComponent, MarketplacesComponent],
   imports: [
     ExtrasModule,
     CKEditorModule,
     FormsModule,
+    MatTableModule,
+    MatTabsModule,
     CommonModule,
     
     RouterModule.forChild([
@@ -45,6 +49,10 @@ import { CurrencyComponent } from './currency/currency.component';
       {
         path: 'kategoriler',
         component: CategoriesComponent,
+      },
+      {
+        path: 'pazar-yerleri',
+        component: MarketplacesComponent,
       },
       { path: 'urunler/:categoryId', component: ProductsComponent },
       { path: 'urun-ekle/:productId', component: ProductAddComponent }
