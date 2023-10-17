acb2060db368eaf45154522d16a3fac3e5dc04cb
mehmetozturk.ankabeta <mehmet.ozturk@ankabeta.com.tr>
2023-10-17 20:50:18 +0300

    :coffee: : Anket oluşturma taslak yapılıyor

--- src/app/pages/poll/poll.component.ts
+++ src/app/pages/poll/poll.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { ModalComponent, ModalConfig } from 'src/app/_metronic/partials';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 export interface Pool {
   id: number;
@@ -19,7 +21,55 @@ interface Question {
   styleUrls: ['./poll.component.scss']
 })
 export class PollComponent {
-  displayedColumns: string[] = ['Id', 'Title', 'LastDate' , 'Action'];
+  startDate: Date = new Date(1991, 1);
+
+  sektorler = new MatTableDataSource([
+    { id: 1, tip: "Gıda" },
+    { id: 2, tip: "İnşaat" },
+    { id: 3, tip: "Tekstil" },
+    { id: 4, tip: "Kozmetik" },
+    { id: 5, tip: "Teknoloji" },
+    { id: 6, tip: "Hububat (Tahıl)" },
+  ])
+  ulkeler = new MatTableDataSource([
+    { id: 1, tip: "Türkiye" },
+    { id: 2, tip: "Amerika" },
+    { id: 3, tip: "Çin" },
+    { id: 4, tip: "Rusya" },
+    { id: 5, tip: "Fransa" },
+  ])
+  oranlar = new MatTableDataSource([
+    { id: 1, tip: "%30" },
+    { id: 2, tip: "%40" },
+    { id: 3, tip: "%50" },
+    { id: 4, tip: "%60" },
+    { id: 5, tip: "%70" },
+    { id: 6, tip: "%80" },
+    { id: 7, tip: "%90" },
+    { id: 8, tip: "%100" },
+  ])
+  selectedSektor: string = 'Gıda';
+  selectedUlke: string = 'Türkiye';
+  selectedOran: string = '%30';
+
+  endDateChange(item: any) {
+    this.startDate = new Date(item.target.value)
+
+    this.getApplicationsByFilter();
+  }
+  getApplicationsByFilter() {
+    throw new Error('Method not implemented.');
+  }
+  changesektor(event: any) {
+    this.selectedSektor = event.target.value; // Seçilen değeri değişkene ata
+  }
+  changeulke(event: any) {
+    this.selectedUlke = event.target.value; // Seçilen değeri değişkene ata
+  }
+  changeoran(event: any) {
+    this.selectedOran = event.target.value; // Seçilen değeri değişkene ata
+  }
+  displayedColumns: string[] = ['Id', 'Title', 'LastDate', 'Action'];
   Pools: Pool[] = [
     {
       id: 1,
@@ -32,9 +82,9 @@ export class PollComponent {
       ]
     }
   ]
-  counter:number[]=[1]
+  counter: number[] = [1]
   selectedPool: Pool | null = null
-  createModalConfig: ModalConfig={
+  createModalConfig: ModalConfig = {
     modalTitle: "Anket Oluştur",
     closeButtonLabel: 'Gönder',
     onDismiss: () => {
@@ -44,24 +94,28 @@ export class PollComponent {
     hideCloseButton: () => true
   }
 
-  successModalConfig: ModalConfig={
-    modalTitle:"Başarılı",
-    closeButtonLabel:"Kapat"
+  successModalConfig: ModalConfig = {
+    modalTitle: "Başarılı",
+    closeButtonLabel: "Kapat"
   }
 
-  detailModalConfig: ModalConfig={
-    modalTitle:"Anket Detayı",
-    closeButtonLabel:"Kapat"
+  detailModalConfig: ModalConfig = {
+    modalTitle: "Anket Detayı",
+    closeButtonLabel: "Kapat"
   }
 
-  deleteModalConfig:ModalConfig={
-    modalTitle:"Sil",
+  deleteModalConfig: ModalConfig = {
+    modalTitle: "Sil",
   }
 
   @ViewChild('createModal') private createModalComponent: ModalComponent;
   @ViewChild('successModal') private successModalComponent: ModalComponent;
   @ViewChild('detailModal') private detailModalComponent: ModalComponent;
   @ViewChild('deleteModal') private deleteModalComponent: ModalComponent;
+  selectedType: any;
+  change(item: any) {
+    this.selectedType = item.target.value;
+  }
 
   openCreateModal() {
     this.createModalComponent.open();
@@ -70,17 +124,17 @@ export class PollComponent {
     this.createModalComponent.close();
     this.successModalComponent.open();
   }
-  openDetailModal(pool:Pool){
+  openDetailModal(pool: Pool) {
     this.selectedPool = pool;
     this.detailModalComponent.open();
   }
-  openDeleteModal(){
+  openDeleteModal() {
     this.deleteModalComponent.open();
   }
-  closeDeleteModal(){
+  closeDeleteModal() {
     this.deleteModalComponent.close();
   }
   counterUpdate() {
-    this.counter.push(this.counter.length+1);
-}
+    this.counter.push(this.counter.length + 1);
+  }
 }
